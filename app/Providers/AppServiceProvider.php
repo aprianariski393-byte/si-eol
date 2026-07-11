d8440ed2470f5c605fb22889b8e39555a3fac55d
Andereyan Muhammat <andereyan@gmail.com>
2026-07-12 02:12:01 +0800

    feat: implement plaintext authentication provider and register sidebar user card hook

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\EloquentUserProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +24,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Auth::provider('plaintext', function ($app, array $config) {
+            return new class($app['hash'], $config['model']) extends EloquentUserProvider {
+                public function validateCredentials(\Illuminate\Contracts\Auth\Authenticatable $user, array $credentials)
+                {
+                    $plain = $credentials['password'];
+                    return $user->getAuthPassword() === $plain;
+                }
+
+                public function rehashPasswordIfRequired(\Illuminate\Contracts\Auth\Authenticatable $user, array $credentials, bool $force = false)
+                {
+                    // Do nothing for plain text passwords to avoid bcrypt errors
+                }
+            };
+        });
+
         // Navigation Top User Card
         FilamentView::registerRenderHook(
             PanelsRenderHook::SIDEBAR_NAV_START,
